92e53908d2215cc368dd17e3df02c7a0e8a67ca4
viper1021 <108946326+viper1021@users.noreply.github.com>
2022-07-08 14:23:47 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,129 @@
+const carrito = document.getElementById('carrito');
+const platillos = document.getElementById('lista-platillos');
+const listaPlatillos = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+
+cargarEventlisteners();
+
+function cargarEventlisteners(){
+    platillos.addEventListener('click', comprarPlatillo);
+    carrito.addEventListener('click', eliminarPlatillo);
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);  
+    document.addEventListener('DOMContentLoaded', leerLocalStorange);
+}
+
+function comprarPlatillo(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+         const platillo = e.target.parentElement.parentElement;
+         leerDatosPlatillo(platillo);
+    }
+}
+
+function leerDatosPlatillo(platillo){
+    const infoplatillo = {
+        imagen: platillo.querySelector('img').src,
+        titulo: platillo.querySelector('h4').textContent,
+        precio: platillo.querySelector('.precio span').textContent,
+        id: platillo.querySelector('a').getAttribute('data-id')
+    }
+    insertarCarrito(infoPlatillo);  
+}
+
+function insertarCarrito(platillo) {
+    const row = document.createElement('tr');
+    row.innerHTML= `
+    <td>
+    <img src="${platillo.imagen}" width=100>
+    </td>
+    <td>${platillo.titulo}</td>
+    <td>${platillo.precio}</td>
+    <td>
+    <a href="#" class="borrar-platillo" data-id="${platillo.id}">X</a>
+    </td>
+    `;
+    listaPlatillos.appendChild(row);
+    guardarPlatilloLocalStorage(platillo);
+}
+
+function eliminarPlatillo(e) {
+    e.preventDefault();
+
+    let platillo,
+        platilloId;
+
+    if(e.target.classList.contains('borrar-platillo')) {
+        e.target.parentElement.parentElement.remove();
+        platillo = e.target.parentElement.parentElement;
+        platilloId = platillo.querySelector('a').getAttribute('data-id');
+    }
+    eliminarPlatilloStorage(platilloId);
+}
+
+function vaciarCarrito(){
+    while(listaPlatillos.firstChild){
+        listaPlatillos.removeChild(listaPlatillos.firstChild);
+    }
+    vaciarLocalStorage();
+
+    return false;
+}
+
+function guardarPlatilloLocalStorage(platillo) {
+    let platillos;
+
+    platillos = obtenerPlatilloslocalStorage();
+    platillos.push(platillo);
+
+    localStorage.setItem('platillos', JSON.stringify(platillos));
+}
+
+function obtenerPlatilloslocalStorage() {
+    let platillosLS;
+
+    if(localStorage.getItem('platillos') === null) {
+        platillosLS=[];
+    } else{
+        platillosLS = JSON.parse(localStorage.getItem('platillos'));
+    }
+    return platillosLS;
+}
+
+function leerLocalStorange(){
+    let platillosLS;
+
+    platillosLS = obtenerPlatilloslocalStorage();
+
+    platillosLS.forEach(function(platillo) {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>
+                    <img src="${platillo.imagen}" width=100>
+                </td>
+                <td>${platillo.titulo}</td>
+                <td>${platillo.precio}</td>
+                <td>
+                    <a href="#" class="borrar-platillo" data-id="${platillo.id}">X</a>
+                </td>
+            `;
+            listaPlatillos.appendChild(row);
+    });
+}
+
+function eliminarPlatilloLocalStorage(platillo) {
+    let platillosLS;
+    platillosLS = obtenerPlatilloslocalStorage();
+
+    platillosLS.forEach(function(platillosLS, index) {
+        if(platilloLS.id === platillo){
+            platillosLS.splice(index, 1);
+        }
+
+    });
+
+    localStorage.setItem('platillos', JSON.stringify(platillosLS));
+}
+
+function vaciarLocalStorage() {
+    localStorage.clear();
+}
